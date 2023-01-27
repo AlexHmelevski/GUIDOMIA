49a800c09de578052f405fcfa86e6ad63dbef560
alex-crowe <71283686+alex-crowe@users.noreply.github.com>
2023-01-27 08:31:57 -0500

    Initial Commit

---
+++ GUIDOMIA/GUIDOMIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GUIDOMIAApp.swift
+//  GUIDOMIA
+//
+//  Created by Alex Crowe on 2023-01-27.
+//
+
+import SwiftUI
+
+@main
+struct GUIDOMIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GUIDOMIAUITests/GUIDOMIAUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GUIDOMIAUITestsLaunchTests.swift
+//  GUIDOMIAUITests
+//
+//  Created by Alex Crowe on 2023-01-27.
+//
+
+import XCTest
+
+final class GUIDOMIAUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
